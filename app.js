f1de4f62a56240a223bb32776a2317f0b96057c4
Ignat Seliun <seliunignat@gmail.com>
2021-03-22 23:48:04 +0300

    Prepare to deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -11,16 +12,26 @@ app.use('/api/text', require('./routes/text.routes'))
 app.use('/api/comment', require('./routes/comment.routes'))
 app.use('/api/image', require('./routes/image.routes'))
 
-const PORT = config.get('port') || 5000
+const PORT =  process.env.PORT || config.get('port') || 5000 //Step 1
 
 async function start()
 {
     try {
+        //Step 2
         await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         })
+        //Step 3
+        if(process.env.NODE_ENV === 'production'){
+            app.use(express.static( 'client/build' ))
+
+            app.get('*', (req, res) => {
+                res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+            })
+        }
+
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
